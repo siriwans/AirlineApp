a5b0ff0c6d237fc6aea60bf2433776e3153d3173
Chanelle <ckaith97@yahoo.com>
2019-05-25 17:29:13 -0700

    added gui

--- src/PreparedStmtMoviesExample.java
+++ src/PreparedStmtMoviesExample.java
@@ -6,10 +6,10 @@ public class PreparedStmtMoviesExample
 {
     // JDBC Driver Name & Database URL
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String JDBC_DB_URL = "jdbc:mysql://toshikuboi.net:3306/ssereesa";
+    static final String JDBC_DB_URL = "jdbc:mysql://toshikuboi.net:3306/cmosquer";
     // JDBC Database Credentials
-    static final String JDBC_USER = "ssereesa";
-    static final String JDBC_PASS = "014479305";
+    static final String JDBC_USER = "cmosquer";
+    static final String JDBC_PASS = "011087566"; //"014479305";
 
     public static void main(String[] args)
     {
@@ -21,9 +21,10 @@ public class PreparedStmtMoviesExample
             Connection connObj = DriverManager.getConnection(JDBC_DB_URL, JDBC_USER, JDBC_PASS);
             //create prepared sql statement
             PreparedStatement prepStatement = connObj.prepareStatement(
-                    "SELECT * FROM Movies,StarsIn WHERE Movies.mid=StarsIn.mid AND director=?");
+                    "SELECT * FROM Player");
+                    //"SELECT * FROM Movies,StarsIn WHERE Movies.mid=StarsIn.mid AND director=?");
             //plug in a parameter
-            prepStatement.setString(1, "James Cameron");
+            //prepStatement.setString(1, "James Cameron");
             //execute the statement
             ResultSet resObj = prepStatement.executeQuery();
             //loop through the result set
@@ -31,8 +32,9 @@ public class PreparedStmtMoviesExample
             {
                 System.out.println(
                         //you have to know the data type of each attribute
-                        "title: " + resObj.getString("title") + ", year: " + resObj.getInt("year")
-                                + ", actor: " + resObj.getString("sname"));
+                        "player_id: " + resObj.getInt("id") + ", first: " + resObj.getString("first")
+                                + ", last: " + resObj.getString("last"));
+
             }
             connObj.close();
         }
