0d26efdd9b1e0dd629d67304c83cd14a3604c552
Siriwan Sereesathien <siriwansereesathien@gmail.com>
2019-06-04 14:55:14 -0700

    update create

--- sqlstatements.sql
+++ sqlstatements.sql
@@ -8,12 +8,6 @@ CREATE TABLE flights(
     FOREIGN KEY (DestAirport) REFERENCES airports(AirportCode),
     FOREIGN KEY (Airline) REFERENCES airlines(Id));
 
-CREATE TABLE flightinfo(
-  airline INTEGER NOT NULL,
-  arrival DATETIME NOT NULL,
-  departure DATETIME NOT NULL,
-  PRIMARY KEY(Airline));
-
 CREATE TABLE airports(
 	City VARCHAR(50) NOT NULL,
 	AirportCode VARCHAR(3) NOT NULL,
@@ -30,10 +24,65 @@ CREATE TABLE airlines(
 	PRIMARY KEY (Id));
 
 CREATE TABLE planes(
-  id INTEGER NOT NULL AUTO_INCREMENT,
-  capacity INTEGER NOT NULL,
-  first INTEGER NOT NULL,
-  business INTEGER NOT NULL,
-  economy INTEGER NOT NULL,
-  full TINYINT DEFAULT 0,
-  PRIMARY KEY(id));
+    Id INTEGER NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(Id));
+
+CREATE TABLE flightInfo(
+    FlightNo INTEGER NOT NULL,
+    Airline INTEGER NOT NULL,
+    PlaneId INTEGER NOT NULL,
+    Arrival DATETIME NOT NULL,
+    Departure DATETIME NOT NULL,
+    SeatNo INTEGER NOT NULL,
+    PRIMARY KEY(FlightNo, Airline, PlaneId),
+    FOREIGN KEY (FlightNo) REFERENCES flights(FlightNo),
+    FOREIGN KEY (Airline) REFERENCES airlines(Id),
+    FOREIGN KEY (PlaneId) REFERENCES planes(Id));
+
+CREATE TABLE seatings(
+    PlaneId INTEGER NOT NULL
+    SeatNo INTEGER NOT NULL
+    Class ENUM("First", "Business", "Economy") NOT NULL,
+    Occupied ENUM("Yes", "No") NOT NULL,
+    PRIMARY KEY(PlaneId, SeatNo),
+    FOREIGN KEY (PlaneId) REFERENCES planes(Id));
+
+CREATE TABLE creditCards(
+    CardNo INTEGER NOT NULL,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    CCV INTEGER NOT NULL,
+    EXP DATE NOT NULL,
+    PRIMARY KEY(CardNo));
+
+CREATE TABLE customers(
+    Id INTEGER NOT NULL,
+    PassportNo VARCHAR(20) NOT NULL,
+    Country VARCHAR(20) NOT NULL,
+    FirstName INTEGER NOT NULL,
+    LastName INTEGER NOT NULL,
+    PRIMARY KEY(Id));
+
+CREATE TABLE bookings(
+    ReservationNo INTEGER NOT NULL,
+    Customer INTEGER NOT NULL,
+    CardNo INTEGER NOT NULL,
+    FlightNo INTEGER NOT NULL,
+    Cancelled ENUM("Yes", "No") NOT NULL,
+    PRIMARY KEY(ReservationNo),
+    FOREIGN KEY(Customer) REFERENCES customers(Id),
+    FOREIGN KEY(CardNo) REFERENCES creditCards(Id),
+    FOREIGN KEY(FlightNo) REFERENCES flights(FlightNo));
+
+CREATE TABLE transactions(
+    Id INTEGER NOT NULL,
+    creditCard INTEGER NOT NULL,
+    Amount REAL NOT NULL,
+    Date DATE NOT NULL,
+    Booking INTEGER NOT NULL,
+    Refunded ENUM("Yes", "No") NOT NULL,
+    PRIMARY KEY(Id),
+    FOREIGN KEY(creditCard) REFERENCES creditCards(CardNo),
+    FOREIGN KEY(Booking) REFERENCES bookings(Id));
+
+
