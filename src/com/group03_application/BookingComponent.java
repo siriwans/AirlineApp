6053984769073a6aa43b7f8cd587d0057dfabbe0
Chanelle <ckaith97@yahoo.com>
2019-06-11 16:02:08 -0700

    done. list of reservations

--- src/com/group03_application/BookingComponent.java
+++ src/com/group03_application/BookingComponent.java
@@ -1,9 +1,30 @@
 package com.group03_application;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class BookingComponent {
     private JLabel lblReservationNo;
     private JTextField hereIsOtherInformationTextField;
     private JLabel txtReservationNo;
+    private JPanel pnlMain;
+    private JButton btnCancel;
+
+    public BookingComponent(int i) {
+
+        btnCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public JPanel getPnlMain() {
+        return pnlMain;
+    }
+
+
+
 }
