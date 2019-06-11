c73907db4f246a3ea54ef54045eb5f5b24b0aacc
cedaley <cedaley@calpoly.edu>
2019-06-11 16:59:53 -0700

    Entered in deleting a booking trigger into db cancelling a booking will remove the cancelled seat, remove one from the count of the flight and will refund the transaction

--- triggers.sql
+++ triggers.sql
@@ -65,3 +65,44 @@ END $
 DELIMITER;
 
 --cancelling a booking
+
+DELIMITER $
+CREATE TRIGGER `update_cancelled_flights`
+AFTER UPDATE ON `bookings`
+FOR EACH ROW
+BEGIN
+  UPDATE transactions SET refunded = 'Yes'
+  WHERE NEW.cancelled = 'Yes'
+  AND booking = NEW.reservationNo;
+END $
+DELIMITER;
+
+DELIMITER $
+CREATE TRIGGER `remove_cancelled_flight`
+AFTER UPDATE ON `bookings`
+FOR EACH ROW
+BEGIN
+  UPDATE planes p SET p.count = (p.count - 1)
+  WHERE p.id = (
+    SELECT planeid FROM flightinfo
+    WHERE airline = NEW.airline
+    AND flightno = NEW.flightno
+  );
+END $
+DELIMITER;
+
+DELIMITER $
+CREATE TRIGGER `remove_cancelled_seat`
+AFTER UPDATE ON `bookings`
+FOR EACH ROW
+BEGIN
+  UPDATE seatings SET custumer = NULL
+  WHERE planeid = (
+      SELECT planeid FROM flightinfo
+      WHERE airline = NEW.airline
+      AND flightno = NEW.flightno )
+  AND seatno = NEW.seatno;
+END $
+DELIMITER;
+
+
